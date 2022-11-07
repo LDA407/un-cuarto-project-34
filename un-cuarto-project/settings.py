ccb57d79715d4060bfdef15152cf4e6163326397
LDA407 <88114886+LDA407@users.noreply.github.com>
2022-11-07 19:47:57 -0300

    Djd

--- un-cuarto-project/settings.py
+++ un-cuarto-project/settings.py
@@ -94,11 +94,7 @@ import dj_database_url
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(env('DATABASE_URL'),conn_max_age=600)
 }
 
 
