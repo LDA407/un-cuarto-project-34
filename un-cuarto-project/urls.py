3664f06e2b0430f244f9aab51c2316d3963f85fa
LDA407 <88114886+LDA407@users.noreply.github.com>
2022-11-07 18:43:28 -0300

    Update urls.py

--- un-cuarto-project/urls.py
+++ un-cuarto-project/urls.py
@@ -11,15 +11,6 @@ urlpatterns = [
     path('', include('un_cuarto.urls', namespace='un_cuarto')),
 ]
 
-# urlpatterns += [
-#     re_path(r'^static/(?P<path>.*)$', serve,{
-#         "document_root" : settings.STATIC_ROOT
-#     }),
-#     re_path(r'^media/(?P<path>.*)$', serve,{
-#         "document_root" : settings.MEDIA_ROOT
-#     })
-# ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
