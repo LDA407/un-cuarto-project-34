f41856a2d685e308ad247e76d287a3c4ff83d93e
LDA407 <ALENCINA407@GMAIL.COM>
2022-11-07 17:00:11 -0300

    setaosiijd

--- un-cuarto-project/settings.py
+++ un-cuarto-project/settings.py
@@ -197,6 +197,8 @@ if not DEBUG:
     # SECURE_REDIRECT_EXEMPT = []
     ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 3
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     # # django-ckeditor will not work with S3 through django-storages without this line in settings.py
